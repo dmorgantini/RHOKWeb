374d0233c546ca3f45e66165ba587769d867cdc1
David Morgantini <dmorgantini@gmail.com>
2011-12-20 16:43:05 +0000

    fixed port issue

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.get("/", function(req, resp) {
 
 var port = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
