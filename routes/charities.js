9b421e5fb6edbed65e9481468b9b2ecd219c26d3
David Morgantini <dmorgantini@gmail.com>
2012-01-12 22:24:39 +0000

    added baseURI to link

--- routes/charities.js
+++ routes/charities.js
@@ -5,19 +5,22 @@ var charityModel = require('../model/Charity.js');
 
 
 exports.jsonList = function (req, res) {
-    charityModel.Charity.find({'charityState': "accepted"}, function(err, docs){ // TODO: change this to "approved" when approval functionality completed
-        if (!err)
-        {
+    charityModel.Charity.find({'charityState':"accepted"}, function (err, docs) {
+        if (!err) {
             var json = [];
-            docs.forEach(function(elem){
-                json.push({ "id": elem._id, "name": elem.name, "donationInstructions": elem.donationInstructions})
+            docs.forEach(function (elem) {
+                json.push({
+                    "id":elem._id,
+                    "name":elem.name,
+                    "donationInstructions":elem.donationInstructions,
+                    "baseURI": (/^[^#]*?:\/\/(.*?)(\/.*)?$/).exec(elem.website)[1]
+                });
             });
             return res.send(json);
         }
-        else
-        {
+        else {
             console.log(err);
-            return res.send({'error': 'There was an error'}, 400);
+            return res.send({'error':'There was an error'}, 400);
         }
     });
 
